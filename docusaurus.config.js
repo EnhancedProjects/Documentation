fb1392c003fec47f09c524c1682897fa210a6319
KPGTB <kpg2k6@gmail.com>
2025-01-01 18:44:13 +0100

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -97,7 +97,7 @@ const config = {
 							},
 							{
 								label: "JavaDocs",
-								hreft: "https://javadocs.projectenhanced.dev",
+								href: "https://javadocs.projectenhanced.dev",
 							},
 						],
 					},
